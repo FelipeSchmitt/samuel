f18bbbb5b4382c6d4538758a9f98f8ff4942f1b8
FelipeSchmitt <50593950+FelipeSchmitt@users.noreply.github.com>
2020-07-10 09:49:36 -0300

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,14 +1,14 @@
 var tempo = 3000;
 
 function slide1(){
-    document.getElementById("slider").src="images/slide1.png"
+    document.getElementById("slider").src="images/slide1.jpg"
     setTimeout("slide2()", tempo)
 }
 function slide2(){
-    document.getElementById("banner").src="images/slide2.png"
+    document.getElementById("banner").src="images/slide2.jpg"
     setTimeout("slide3()", tempo)
 }
 function slide3(){
-    document.getElementById("banner").src="images/slide3.png"
+    document.getElementById("banner").src="images/slide3.jpg"
     setTimeout("slide1()", tempo)
 }
