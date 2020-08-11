1d6ace6e1b0bde66f467ec07d72ab088ba70ab0b
FelipeSchmitt <50593950+FelipeSchmitt@users.noreply.github.com>
2020-08-11 13:23:23 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function showhide(){
+    var showhide = document.getElementById("password")
+    console.log(showhide)
+    if(showhide.type == "password"){
+        showhide.type="text"
+    }else{
+        showhide.type="password"
+    }
+}
